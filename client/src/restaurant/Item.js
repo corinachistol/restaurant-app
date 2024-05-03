9328c0ba86b0712a548bb7367bdc84afb778799f
corina <chistolcorina19@gmail.com>
2024-05-03 12:38:29 +0300

    sort added part5

--- client/src/restaurant/Item.js
+++ client/src/restaurant/Item.js
@@ -4,7 +4,7 @@ const Item = ({data}) => {
     console.log(data)
     return ( 
         <li className="p-2 border-solid border-b-2 border-zinc-500 ">
-            <h2>{data.name}</h2>
+            <h2 className="font-bold">{data.name}</h2>
             <img src={data.image} alt={data.name} className="w-72 h-72 object-cover"/>
             <Money price={data.price} key={data.price.id} />
         </li>
--- client/src/restaurant/Menu.js
+++ client/src/restaurant/Menu.js
@@ -1,16 +1,39 @@
 import Item from "./Item";
+import { BsChevronCompactDown, BsChevronCompactUp } from "react-icons/bs";
+import { useEffect, useState } from "react";
 
 
-const Menu = (props) => {
-    let data = props.data
-    // console.log(data);
+const Menu = ({data}) => {
+  let [itemsData,setItemsData] = useState(data)
+
+
+  const sortItemsDesc = () => {
+    itemsData.sort((i1, i2) => i2.price.amount - i1.price.amount)
+    setItemsData([...itemsData])
+  }
+  const sortItemsAsc = () => {
+    itemsData.sort((i1, i2) => i1.price.amount - i2.price.amount)
+    setItemsData([...itemsData])
+  }
+  
+
+
   return (
-    <ul className="p-4">
-        {data && data.map(item => (
-          <Item data={item} key={item.id} />
+    <div>
+      <button onClick={sortItemsDesc}><BsChevronCompactDown/> </button>
+      <button onClick={sortItemsAsc}><BsChevronCompactUp/> </button>
+      {/* 
+      HW1: add ascending sort
+      Hw2: optimize using 1 button */}
 
-        ))}
-    </ul>
+      <ul className="p-4">
+          {data && data.map(itemData => (
+            <Item data={itemData} key={itemData.id} />
+
+          ))}
+      </ul>
+    
+    </div>
   )
 }
 
